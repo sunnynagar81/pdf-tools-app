513f3216ab8723616e87f2e4ca24fe403c524804
sunny nagar <sunnynagar3134@gmail.com>
2026-03-07 09:43:24 +0530

    connect backend render

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://pdf-tools-app-knwd.onrender.com/api/auth/login",
         { email, password }
       );
 
