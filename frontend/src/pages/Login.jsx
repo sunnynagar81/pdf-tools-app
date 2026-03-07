8d98e905e5ea73eafdb6542a314d4d39b03cbbdb
sunny nagar <sunnynagar3134@gmail.com>
2026-03-07 09:47:17 +0530

    fix login api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
     try {
       const res = await axios.post(
-        "https://pdf-tools-app-knwd.onrender.com/api/auth/login",
+        "https://pdf-tools-app-knwd.onrender.com/login",
         { email, password }
       );
 
