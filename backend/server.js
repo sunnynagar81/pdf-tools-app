3c06f794d0b515bd7cf86028642b61fc8f96d0c1
sunny nagar <sunnynagar3134@gmail.com>
2026-03-07 10:18:49 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const pdfTools = require("./routes/pdfTools");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials:true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
